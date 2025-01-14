870bc3df1d2445857d9ceec10c5ebeabb8ef2d40
asifahmed1810 <asifahmed18oct@gmail.com>
2025-01-14 17:00:22 +0600

    initialize firebase

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZXQSJ7bknQBUnFIbYmHyOHKxQA05chzg",
+  authDomain: "mediflow-27ae3.firebaseapp.com",
+  projectId: "mediflow-27ae3",
+  storageBucket: "mediflow-27ae3.firebasestorage.app",
+  messagingSenderId: "37879299517",
+  appId: "1:37879299517:web:a5bec483494939da7426d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
