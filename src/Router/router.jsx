05b136351582c42308cec43b2a06e545191de06e
asifahmed1810 <asifahmed18oct@gmail.com>
2025-01-14 11:59:28 +0600

    Added navbar

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -3,11 +3,22 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import MainLayOut from '../LayOut/MainLayOut';
+import Home from '../pages/Home/Home';
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayOut></MainLayOut>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            
+        }
+      ]
     },
   ]);
 
