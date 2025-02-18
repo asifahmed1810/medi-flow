282ca1c76707121612f5947dd540700be99bae5e
asifahmed1810 <asifahmed18oct@gmail.com>
2025-02-18 10:39:39 +0600

    Update error page

--- src/pages/ErrorPage/Errorpage.jsx
+++ src/pages/ErrorPage/Errorpage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Errorpage = () => {
     return (
-        <div className='flex  justify-center mt-72 w-11/12 mx-auto '>
+        <div className='flex flex-col items-center justify-center mt-72 w-11/12 mx-auto '>
             <h1 className='text-2xl'>   <span className='font-bold '>404</span> | <span>This page could not be found.</span> </h1>
+            <div className='mt-8'>
+                <h2 className='text-xl'>Go to <span><Link to={'/'}><button className='btn font-semibold'>Home</button></Link></span> page</h2>
+            </div>
+            
         </div>
     );
 };
